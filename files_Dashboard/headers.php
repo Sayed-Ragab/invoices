e0a5c15ddf76fd0290579df70627499f68e1bb5d
sayed <138671539+Sayed-Ragab@users.noreply.github.com>
2025-12-30 13:49:57 +0200

    Update headers.php

--- files_Dashboard/headers.php
+++ files_Dashboard/headers.php
@@ -114,12 +114,7 @@
 												</div>
 											</div>
 										</div>
-										<a class="dropdown-item" href=""><i class="bx bx-user-circle"></i>Profile</a>
-										<a class="dropdown-item" href=""><i class="bx bx-cog"></i> Edit Profile</a>
-										<a class="dropdown-item" href=""><i class="bx bxs-inbox"></i>Inbox</a>
-										<a class="dropdown-item" href=""><i class="bx bx-envelope"></i>Messages</a>
-										<a class="dropdown-item" href=""><i class="bx bx-slider-alt"></i> Account Settings</a>
-										<a class="dropdown-item" href="../Dashboard/logout.php"><i class="bx bx-log-out"></i> Sign Out</a>
+										<a class="dropdown-item" href="../Dashboard/logout.php"><i class="bx bx-log-out"></i> تسجيل الخروج</a>
 									</div>
 								</div>
 								<div class="dropdown main-header-message right-toggle">
